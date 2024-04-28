a61275f8b5d7669b3a0ed1a6a5e70c0a99301a00
diamondburned <diamond@libdb.so>
2024-04-27 23:19:33 -0700

    Fix persist.Value type awkwardness, add WrapMustX fns

--- must.go
+++ must.go
@@ -22,6 +22,9 @@ func NewMustMap[K, V any](driverOpener DriverOpenFunc, path string) (MustMap[K, 
 	return MustMap[K, V]{m}, nil
 }
 
+// WrapMustMap wraps a Map in a MustMap.
+func WrapMustMap[K, V any](m Map[K, V]) MustMap[K, V] { return MustMap[K, V]{m} }
+
 // Load returns the value associated with the key, or false if the key is not
 // found.
 func (m MustMap[K, V]) Load(key K) (V, bool) {
@@ -74,21 +77,24 @@ func (m MustMap[K, V]) Delete(key K) {
 
 // MustValue wraps a value and guarantees that no errors will be returned from
 // the value's methods.
-type MustValue[K, V any] struct {
-	Value[K, V]
+type MustValue[V any] struct {
+	Value[V]
 }
 
 // NewMustValue returns a new MustValue. It has the same exact signature as
 // NewValue, and the user must still handle errors as they would with NewValue.
-func NewMustValue[V any](driverOpener DriverOpenFunc, path string) (MustValue[valueKeyT, V], error) {
+func NewMustValue[V any](driverOpener DriverOpenFunc, path string) (MustValue[V], error) {
 	v, err := NewValue[V](driverOpener, path)
 	if err != nil {
-		return MustValue[valueKeyT, V]{}, err
+		return MustValue[V]{}, err
 	}
-	return MustValue[valueKeyT, V]{v}, nil
+	return MustValue[V]{v}, nil
 }
 
-func (m MustValue[K, V]) Load() (V, bool) {
+// WrapMustValue wraps a Value in a MustValue.
+func WrapMustValue[V any](value Value[V]) MustValue[V] { return MustValue[V]{value} }
+
+func (m MustValue[V]) Load() (V, bool) {
 	v, ok, err := m.Value.Load()
 	if err != nil {
 		panic(fmt.Sprintf("MustValue cannot load: %v", err))
@@ -96,13 +102,13 @@ func (m MustValue[K, V]) Load() (V, bool) {
 	return v, ok
 }
 
-func (m MustValue[K, V]) Store(value V) {
+func (m MustValue[V]) Store(value V) {
 	if err := m.Value.Store(value); err != nil {
 		panic(fmt.Sprintf("MustValue cannot store: %v", err))
 	}
 }
 
-func (m MustValue[K, V]) LoadAndDelete() (V, bool) {
+func (m MustValue[V]) LoadAndDelete() (V, bool) {
 	v, loaded, err := m.Value.LoadAndDelete()
 	if err != nil {
 		panic(fmt.Sprintf("MustValue cannot load and delete: %v", err))
@@ -110,7 +116,7 @@ func (m MustValue[K, V]) LoadAndDelete() (V, bool) {
 	return v, loaded
 }
 
-func (m MustValue[K, V]) LoadOrStore(value V) (actual V, loaded bool) {
+func (m MustValue[V]) LoadOrStore(value V) (actual V, loaded bool) {
 	v, loaded, err := m.Value.LoadOrStore(value)
 	if err != nil {
 		panic(fmt.Sprintf("MustValue cannot load or store: %v", err))
@@ -118,7 +124,7 @@ func (m MustValue[K, V]) LoadOrStore(value V) (actual V, loaded bool) {
 	return v, loaded
 }
 
-func (m MustValue[K, V]) Delete() {
+func (m MustValue[V]) Delete() {
 	if err := m.Value.Delete(); err != nil {
 		panic(fmt.Sprintf("MustValue cannot delete: %v", err))
 	}
